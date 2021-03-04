1ab67d30306022a219fcefa86fd15a09958edfe1
GuchiAtWork <52662258+GuchiAtWork@users.noreply.github.com>
2021-03-04 19:35:10 +0900

    Revert "starting to implment home page"

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,13 +1,4 @@
-import 'package:flutter/foundation.dart';
-import 'package:uuid/uuid.dart';
-
-var uuid = Uuid();
-
 class User {
-  User(this.name);
-
-  @required
-  String id = uuid.v4();
-  @required
-  String name;
+  final String uid;
+  User({this.uid});
 }
