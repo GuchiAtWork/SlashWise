3451540cb8995ad81d3cdb87159f7ec20a82b2e0
tNahoko <nahoko.toyota@gmail.com>
2021-03-08 13:55:37 +0900

    display group data from Firebase in Home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:flutter/material.dart";
-import 'package:slash_wise/models/dbUser.dart';
+import 'package:slash_wise/models/dbGroup.dart';
+// import 'package:slash_wise/models/dbUser.dart';
 import 'package:slash_wise/models/group.dart';
-import 'package:slash_wise/screens/home/user_list.dart';
+import 'package:slash_wise/screens/home/group_list.dart';
 import 'package:slash_wise/services/auth.dart';
 import 'package:provider/provider.dart';
-import 'package:slash_wise/services/dbServiceUser.dart';
+import 'package:slash_wise/services/dbServiceGroup.dart';
 //import 'package:slash_wise/widgets/group_list.dart';
 //import 'package:slash_wise/widgets/new_group.dart';
 
@@ -23,34 +24,34 @@ class _HomeState extends State<Home> {
     Group('CC17')
   ];
 
-  void _addNewGroup(String name) {
-    final newGroup = Group(name);
+  // void _addNewGroup(String name) {
+  //   final newGroup = Group(name);
 
-    setState(() {
-      _groups.add(newGroup);
-    });
-  }
+  //   setState(() {
+  //     _groups.add(newGroup);
+  //   });
+  // }
 
-  void _showAddNewGroup(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (_) {
-        return NewGroup(_addNewGroup);
-      },
-    );
-  }
+  // void _showAddNewGroup(BuildContext context) {
+  //   showModalBottomSheet(
+  //     context: context,
+  //     builder: (_) {
+  //       return NewGroup(_addNewGroup);
+  //     },
+  //   );
+  // }
 
-  void _deleteGroup(String id) {
-    setState(() {
-      _groups.removeWhere((group) => group.id == id);
-    });
-  }
+  // void _deleteGroup(String id) {
+  //   setState(() {
+  //     _groups.removeWhere((group) => group.id == id);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<List<DbUser>>.value(
+    return StreamProvider<List<DbGroup>>.value(
       initialData: List(),
-      value: DatabaseServiceUser().users,
+      value: DatabaseServiceGroup().groups,
       child: Scaffold(
         backgroundColor: Colors.blue[50],
         appBar: AppBar(
@@ -67,10 +68,10 @@ class _HomeState extends State<Home> {
               )
             ]),
         body: GroupList(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => {_showAddNewGroup(context)},
-        ),
+        // floatingActionButton: FloatingActionButton(
+        //   child: Icon(Icons.add),
+        //   onPressed: () => {_showAddNewGroup(context)},
+        // ),
       ),
     );
   }
