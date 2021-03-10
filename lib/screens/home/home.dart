4306f320121acb5abf94275aa003e4d60a433480
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-03-10 23:39:07 +0900

    update home.dart remove streamprovider and remove the streambuilder for setState

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,5 @@
 import "package:flutter/material.dart";
-import 'package:slash_wise/models/dbUser.dart';
 import 'package:slash_wise/services/auth.dart';
-import 'package:provider/provider.dart';
-import 'package:slash_wise/services/dbServiceUser.dart';
 import 'package:slash_wise/widgets/group_list.dart';
 import 'package:slash_wise/widgets/main_drawer.dart';
 
@@ -16,28 +13,24 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<List<DbUser>>.value(
-      initialData: [],
-      value: DatabaseServiceUser().users,
-      child: Scaffold(
-        appBar: AppBar(title: Text("SlashWise"), actions: <Widget>[
-          TextButton.icon(
-            style: TextButton.styleFrom(
-              primary: Colors.black,
-            ),
-            icon: Icon(Icons.person),
-            label: Text("Logout"),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ]),
-        drawer: MainDrawer(),
-        body: GroupList(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => {},
-        ),
+    return Scaffold(
+      appBar: AppBar(title: Text("SlashWise"), actions: <Widget>[
+        TextButton.icon(
+          style: TextButton.styleFrom(
+            primary: Colors.black,
+          ),
+          icon: Icon(Icons.person),
+          label: Text("Logout"),
+          onPressed: () async {
+            await _auth.signOut();
+          },
+        )
+      ]),
+      drawer: MainDrawer(),
+      body: GroupList(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {},
       ),
     );
   }
--- lib/widgets/group_list.dart
+++ lib/widgets/group_list.dart
@@ -6,57 +6,60 @@ import 'package:slash_wise/models/user_auth.dart';
 import 'package:slash_wise/screens/group_screen.dart';
 import 'package:slash_wise/services/dbServiceGroup.dart';
 
-class GroupList extends StatelessWidget {
+class GroupList extends StatefulWidget {
+  @override
+  _GroupListState createState() => _GroupListState();
+}
+
+class _GroupListState extends State<GroupList> {
   final groupDatabase = DatabaseServiceGroup();
+
+  //await groupDatabase.getGroups(user.uid);
+
+  List<DbGroup> _groupList = [];
+
+  _getGroupList(uid) {
+    groupDatabase.getGroups(uid).then((value) => setState(() {
+          _groupList = value;
+        }));
+  }
 
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<AuthUser>(context);
+    _getGroupList(user.uid);
 
-    return FutureBuilder(
-      future: groupDatabase.getGroups(user.uid),
-      builder: (BuildContext context, AsyncSnapshot<List<DbGroup>> snapshot) {
-        if (snapshot.hasData) {
-          return Container(
-            height: 600,
-            child: snapshot.data.isEmpty
-                ? Center(
-                    child: Text('No Group added yet!, Please Create a Group.'),
-                  )
-                : ListView.builder(
-                    itemBuilder: (_, index) {
-                      return Card(
-                        elevation: 6,
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-                        child: ListTile(
-                          leading: CircleAvatar(
-                            radius: 30,
-                            child: Text('Picture'),
-                          ),
-                          title: Text(snapshot.data[index].name),
-                          subtitle: Text(
-                            DateFormat.yMMMd()
-                                .format(snapshot.data[index].date),
-                          ),
-                          trailing: IconButton(
-                              icon: Icon(Icons.delete_forever),
-                              color: Theme.of(context).errorColor,
-                              onPressed: () =>
-                                  //_deleteGroup(snapshot.data[index].id),
-                                  () {}),
-                          onTap: () => Navigator.pushNamed(
-                              context, GroupScreen.routeName),
-                        ),
-                      );
-                    },
-                    itemCount: snapshot.data.length,
+    return Container(
+      height: 600,
+      child: _groupList.isEmpty
+          ? Column(
+              children: <Widget>[Text('No Group added yet!')],
+            )
+          : ListView.builder(
+              itemBuilder: (_, index) {
+                return Card(
+                  elevation: 6,
+                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Text('Picture'),
+                    ),
+                    title: Text(_groupList[index].name),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(_groupList[index].date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete_forever),
+                      color: Theme.of(context).errorColor,
+                      //onPressed: () => _deleteGroup(_groupList[index].id),
+                    ),
+                    onLongPress: () {},
                   ),
-          );
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
+                );
+              },
+              itemCount: _groupList.length,
+            ),
     );
   }
 }
