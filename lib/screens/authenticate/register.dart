606236c518bf7762c7b1d376ae2742342005cd10
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-03-19 18:46:48 +0900

    fix bug screen size at login and auth screen when opening the keyboard

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -20,104 +20,112 @@ class _RegisterState extends State<Register> {
   String error = "";
   @override
   Widget build(BuildContext context) {
+    final PreferredSizeWidget _appBar =
+        AppBar(title: Text("Welcome to SlashWise"));
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Welcome to SlashWise")
-        ),
-      body: Container(
-        decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage('assets/signin.jpeg'), fit :BoxFit.cover),
-        ),
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Email",
-                      fillColor: Colors.white,
-                      filled: true
-                      ),
-                  validator: (val) => val.isEmpty ? 'Enter an Email Address' : null,
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(height: 40.0),
-                TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Username",
-                      fillColor: Colors.white,
-                      filled: true
-                      ),
-                    validator: (val) =>
-                        val.isEmpty ? 'Enter an username' : null,
-                    onChanged: (val) {
-                      setState(() => username = val);
-                    }),
-                SizedBox(height: 40.0),
-                TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      fillColor: Colors.white,
-                      filled: true
-                      ),
-                    obscureText: true,
-                    validator: (val) => val.length < 6
-                        ? 'Enter an password 6+ chars long'
-                        : null,
-                    onChanged: (val) {
-                      setState(() => password = val);
-                    }),
-                SizedBox(height: 40.0),
-                SizedBox(
-                  width: 400,
-                  child: ElevatedButton(
-                    child: Text("Sign Up",),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        dynamic result =
-                            await _auth.registerWithEmailAndPassword(
-                                email, username, password);
-                        if (result == null) {
-                          setState(() => error = "please supply a valid email");
-                        }
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.indigo[900],
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
-                      elevation: 5.0,
-                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                      textStyle: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold)),
-                  ),
-                ),
-                SizedBox(height: 40.0),
-                SizedBox(
-                  width: 400,
-                  child: ElevatedButton(
-                    child: Text("Log In"),
-                    onPressed: () {
-                      widget.toggleView();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.indigo[700],
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
-                      elevation: 5.0,
-                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                      textStyle: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold)),
-                    ),
-                )
-              ],
-            )
+      appBar: _appBar,
+      body: SingleChildScrollView(
+        child: Container(
+          height: (MediaQuery.of(context).size.height -
+              _appBar.preferredSize.height -
+              MediaQuery.of(context).padding.top),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/signin.jpeg'), fit: BoxFit.cover),
           ),
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Email",
+                        fillColor: Colors.white,
+                        filled: true),
+                    validator: (val) =>
+                        val.isEmpty ? 'Enter an Email Address' : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    },
+                  ),
+                  SizedBox(height: 40.0),
+                  TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Username",
+                          fillColor: Colors.white,
+                          filled: true),
+                      validator: (val) =>
+                          val.isEmpty ? 'Enter an username' : null,
+                      onChanged: (val) {
+                        setState(() => username = val);
+                      }),
+                  SizedBox(height: 40.0),
+                  TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Password",
+                          fillColor: Colors.white,
+                          filled: true),
+                      obscureText: true,
+                      validator: (val) => val.length < 6
+                          ? 'Enter an password 6+ chars long'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      }),
+                  SizedBox(height: 40.0),
+                  SizedBox(
+                    width: 400,
+                    child: ElevatedButton(
+                      child: Text(
+                        "Sign Up",
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          dynamic result =
+                              await _auth.registerWithEmailAndPassword(
+                                  email, username, password);
+                          if (result == null) {
+                            setState(
+                                () => error = "please supply a valid email");
+                          }
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.indigo[900],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50.0)),
+                          elevation: 5.0,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 20),
+                          textStyle: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  SizedBox(height: 40.0),
+                  SizedBox(
+                    width: 400,
+                    child: ElevatedButton(
+                      child: Text("Log In"),
+                      onPressed: () {
+                        widget.toggleView();
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.indigo[700],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50.0)),
+                          elevation: 5.0,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 20),
+                          textStyle: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold)),
+                    ),
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
