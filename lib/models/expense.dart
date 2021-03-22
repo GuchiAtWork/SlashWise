81bf39e88f8bd5a477f8b162207b6b9675a5c972
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-03-22 16:03:48 +0900

    change logo and spash screen

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:slash_wise/models/user.dart';
-import 'package:uuid/uuid.dart';
-
-var uuid = Uuid();
 
 class Expense {
   Expense(this.id, this.name, this.amount, this.date, this.payer, this.groupID,
--- lib/models/group.dart
+++ lib/models/group.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/foundation.dart';
 import '../models/expense.dart';
 import '../models/user.dart';
-import 'package:uuid/uuid.dart';
 
-var uuid = Uuid();
 
 class Group {
   Group(this.id, this.name, this.users, this.expenses, this.date);
